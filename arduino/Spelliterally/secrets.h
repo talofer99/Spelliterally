d3136f6f2496f3fd5e16682ef8d066603ca7743d
Tal OFer <talofer99@hotmail.com>
2019-09-23 12:27:23 +0300

    added secret back

---
+++ arduino/Spelliterally/secrets.h
@@ -0,0 +1,5 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "******"    // replace MySSID with your WiFi network name
+#define SECRET_PASS "******"  // replace MyPassword with your WiFi password
