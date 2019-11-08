444ef1ff2f6ef5b7fbc0f6edd6d3e491189e401f
Tal OFer <talofer99@hotmail.com>
2019-11-08 08:52:06 +0200

    fixing from old to new

--- arduino/Spelliterally/secrets.h
+++
@@ -1,5 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "Tal"    // replace MySSID with your WiFi network name
-#define SECRET_PASS "17121973"  // replace MyPassword with your WiFi password
